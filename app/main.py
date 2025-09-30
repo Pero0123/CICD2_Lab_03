5434f9d510a2073136bcffc53ae4b4a6dc0247b7
Pero <g00422221@atu.ie>
2025-09-30 10:05:13 +0100

    added tests for put

--- app/main.py
+++ app/main.py
@@ -28,14 +28,18 @@ def add_user(user: User):
     return user
 
 
-@app.put("/api/users/", status_code=status.HTTP_201_CREATED)
-def update_user(user: User):
-    for u in users:
-        if(u.user_id == user.user_id):
-            users[users.index(u)] = user 
-            return user
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-
+@app.put("/api/users/{user_id}", status_code=200)
+def update_user(user: User, user_id: int):
+        for u in users:
+            x =0
+            if(u.user_id == user_id):
+                users[users.index(u)] = user 
+                return user
+            else:
+                if (x==0):
+                    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+                    x=1
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 @app.delete("/api/users/{user_id}", status_code = 204)
 def delete_user(user_id: int):
